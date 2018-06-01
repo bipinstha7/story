d9002d55e57e73393c46e52b70268402994ad7e2
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-01 10:25:34 +0545

    update port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port: ${port} `);
 });
