4bf675ce4952a61c6b9ae2cc671e3df358c0616a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-02 14:35:28 +0545

    version 1.0.1 alpha

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://bipins:bipin1s@ds143070.mlab.com:43070/story",
-  googleClientID: "363309509450-nq6hjcddfkm7s7ggn7fc3umceddlso9u.apps.googleusercontent.com",
-  googleCilentSecret: "WKi3sHRMb6TV74f2LE-7ixjz"
+if(process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleCilentSecret: process.env.GOOGLE_CLIENT_SECRET
+}
