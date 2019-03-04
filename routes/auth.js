18df146c1fbb5f0b53c60471dc367381eefa0d63
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-04 11:16:01 +0545

    auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,25 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
 
-router.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
-router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    // Successful authentication
-    res.redirect('/dashboard');
-  });
-
-
-router.get("/auth/verify", (req, res) => {
-  if (req.user) {
-    console.log(req.user);
-  } else {
-    console.log('Not authorized');
-  }
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+	// Successful authentication
+	res.redirect('/dashboard')
 })
 
-router.get("/auth/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
+router.get('/auth/verify', (req, res) => {
+	if (req.user) {
+		console.log(req.user)
+	} else {
+		console.log('Not authorized')
+	}
+})
 
-module.exports = router;
+router.get('/auth/logout', (req, res) => {
+	req.logout()
+	res.redirect('/')
+})
+
+module.exports = router
