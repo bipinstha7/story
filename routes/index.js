8402cb52c090184cfd76e212b76dc9cacd4f8bf2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-04 11:16:38 +0545

    index router for dashboard and about route

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const {ensureAuthenticated, ensureGuest} = require("../helpers/auth");
-const Story = require("../models/Story");
+const { ensureAuthenticated, ensureGuest } = require('../helpers/auth')
+const Story = require('../models/Story')
 
 // home/index route
-router.get("/", ensureGuest, (req, res) => {
-  res.render("index/welcome")
-});
+router.get('/', ensureGuest, (req, res) => {
+	res.render('index/welcome')
+})
 
 // dashboard route
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  Story.find({user: req.user._id})
-  .then(stories => {
-    res.render("index/dashboard", {stories: stories});
-  })
-  .catch(err => console.log("Error finding dashboard stories: /dashboard", err));
-});
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+	Story.find({ user: req.user._id })
+		.then(stories => {
+			res.render('index/dashboard', { stories: stories })
+		})
+		.catch(err => console.log('Error finding dashboard stories: /dashboard', err))
+})
 
 // about route
-router.get("/about", (req, res) => {
-  res.render("index/about");
-});
+router.get('/about', (req, res) => {
+	res.render('index/about')
+})
 
-
-module.exports = router;
+module.exports = router
