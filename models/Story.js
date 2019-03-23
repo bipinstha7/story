caec733987713cee78982dce4d8725c7a4a41697
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-23 11:54:07 +0545

    story model schema

--- models/Story.js
+++ models/Story.js
@@ -1,48 +1,49 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-// create schema
 const StorySchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: "public"
-  },
-  allowComments: {
-    type: Boolean,
-    default: true
-  },
-  body: {
-    type: String,
-    required: true
-  },
-  comments: [{
-    commentBody: {
-      type: String,
-      require: true
-    },
-    commentDate: {
-      type: Date,
-      default: Date.now
-    },
-    commentUser: {
-      type: Schema.Types.ObjectId,
-      ref: "User"
-    }
-  }],
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+	title: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		default: 'public'
+	},
+	allowComments: {
+		type: Boolean,
+		default: true
+	},
+	body: {
+		type: String,
+		required: true
+	},
+	comments: [
+		{
+			commentBody: {
+				type: String,
+				require: true
+			},
+			commentDate: {
+				type: Date,
+				default: Date.now
+			},
+			commentUser: {
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			}
+		}
+	],
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+})
 
-module.exports = mongoose.model("Story", StorySchema, "stories");
+module.exports = mongoose.model('Story', StorySchema, 'stories')
 // 3rd options stories forces mongoose to store story name as stories but not storys
